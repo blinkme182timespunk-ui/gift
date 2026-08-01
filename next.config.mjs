6bc7304d3b7c9dddc52d1ec70416dff95c3c596f
blinkme182timespunk-ui <blinkme182timespunk@gmail.com>
2026-08-01 15:37:04 +0545

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
